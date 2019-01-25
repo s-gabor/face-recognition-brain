287891f1b6cb73fbe42b47b4efa8004c67cc1341
s-gabor <ing.gabor@yahoo.com>
2019-01-25 17:36:01 +0200

    detect faces and display box around the first face in the image

--- src/App.js
+++ src/App.js
@@ -33,12 +33,16 @@ const particlesOption = {
         }
 
 
+
+
+
 class App extends Component {
     constructor() {
         super()
         this.state = {
             input: '',
-            imageUrl: 'https://scontent.fclj2-1.fna.fbcdn.net/v/t1.0-9/50411514_10213490605323527_4480036934273466368_n.jpg?_nc_cat=108&_nc_ht=scontent.fclj2-1.fna&oh=74caa8bbf41e1924a00e7839387844f1&oe=5CFBE8F1'
+            imageUrl: 'https://scontent.fclj2-1.fna.fbcdn.net/v/t1.0-9/50411514_10213490605323527_4480036934273466368_n.jpg?_nc_cat=108&_nc_ht=scontent.fclj2-1.fna&oh=74caa8bbf41e1924a00e7839387844f1&oe=5CFBE8F1',
+            box: {}
         }
     }
 
@@ -48,18 +52,35 @@ class App extends Component {
         console.log(this.state.input)
     }
 
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box; // -> {top_row: ..., left_col: ..., bottom_row: ..., right_col: ...}
+        const image = document.getElementById('input-image');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        // console.log(width, height);
+        // // code here
+        // console.log(data);
+        // console.log(clarifaiFace);
+        return {
+            left_col: width * clarifaiFace.left_col,
+            top_row: height * clarifaiFace.top_row,
+            right_col: width - (width * clarifaiFace.right_col),
+            bottom_row: height - (height * clarifaiFace.bottom_row)
+        }
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({box: box});
+        console.log(box);
+    }
+
     onButtonClick = () => {
         // when clicked it displays the new image 
         this.setState({imageUrl: this.state.input});
         // fetch response from Clarifai API
         app.models.predict(Clarifai.DEMOGRAPHICS_MODEL, this.state.input)
-          .then(response => {
-            // get nested info from clarifai documentation
-            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-          })
-          .catch(err => {
-            console.log(err);
-          });
+          .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+          .catch(err => console.log(err));
     }
     
     render() {
@@ -75,6 +96,7 @@ class App extends Component {
             />
             <FaceRecognition 
                 imageUrl={this.state.imageUrl}
+                box = {this.state.box}
             />
           </div>
         );
@@ -82,3 +104,24 @@ class App extends Component {
 }
 
 export default App;
+
+// <div 
+//     class="bounding-box 021063761-051509696-044575834-06488487" 
+//     style="
+//             top: 21.063761%; 
+//             right: 35.11513000000001%; 
+//             bottom: 48.490303999999995%; 
+//             left: 44.575834%;"
+//     >
+// </div>
+
+
+
+
+
+
+
+
+
+
+
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './FaceRecognition.css';
 import 'tachyons';
 
 
 const FaceRecognition = (props) => {
 	return (
-		<div className='ma4 mt0'>
-			<img alt='' src={props.imageUrl} width='500px' height='auto'/>
+		<div className='ma4 mt0' style={{position: 'absolute'}}>
+			<img id='input-image' alt='' src={props.imageUrl} width='500px' height='auto'/>
+			<div className='bounding-box' style={{top: props.box.top_row, right: props.box.right_col, bottom: props.box.bottom_row, left: props.box.left_col}}></div>
 		</div>
 	);
 }
