e072fe6424c24bcc43500125149d3fa9a107dd73
s-gabor <ing.gabor@yahoo.com>
2019-02-04 11:05:16 +0200

    connect SignIn to back-end

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,16 +3,13 @@ import 'tachyons';
 
 
 const Navigation = (props) => {
-		console.log(props.isSignedIn);
 		if (props.isSignedIn) {
-			console.log('nav is signed in...', props.isSignedIn);
 			return (
 				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 					<p className='f3 dim link pa3 pointer' onClick={() => props.onRouteChange('signout')}>Sign Out</p>
 				</nav>
 			)
 		} else {
-			console.log('nav else...', props.isSignedIn);
 			return (
 				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 					<p className='f3 dim link pa3 pointer' onClick={() => props.onRouteChange('signin')}>Sign In</p>
